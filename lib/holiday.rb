32ab9410fb87ff353b1a3b14f9fc0607875a815a
aeisha simmons <ashicks2017@outlook.com>
2019-05-25 17:15:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.map |holiday, item|
+  holiday_hash.map |season, holiday|
   if item.include? ("BBQ")
     holiday_hash
    end
